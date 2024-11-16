1b96c31abdcbb00898afad8b3f96e4d929bc3b96
baronejavier <74462330+baronejavier@users.noreply.github.com>
2024-11-16 13:18:06 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,16 +5,3 @@ function openModal(modalId) {
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
-
-function makeOrder(sabor) {
-    // Número de teléfono en formato internacional, sin el '+' ni espacios.
-    const phoneNumber = "543794884299"; // Cambia esto por el número real.
-    const message = encodeURIComponent("¡Hola Sabor Natural de Misiones! Me gustaría hacer un pedido de helado de " + sabor + ".");
-    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`;
-
-    // Abrir WhatsApp en una nueva ventana.
-    window.open(whatsappUrl, '_blank');
-
-    alert("¡Agradecemos su confianza! Estamos aquí para ayudarle y responderemos su mensaje a la brevedad."); // Mensaje de éxito.
-    closeModal('modal-' + sabor); // Cerrar el modal después de hacer el pedido.
-}
